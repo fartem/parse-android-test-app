d93470e6eda83958ca8f211dda210fe2b9cbad52
fartem <jaman.smlnsk@gmail.com>
2019-07-23 17:41:20 +0300

    2019-07-23 Dev 0.9.4: Added Share method

--- app/src/main/java/com/smlnskgmail/jaman/randomnotes/components/dialogs/ShareDialog.kt
+++ app/src/main/java/com/smlnskgmail/jaman/randomnotes/components/dialogs/ShareDialog.kt
@@ -1,12 +1,30 @@
 package com.smlnskgmail.jaman.randomnotes.components.dialogs
 
 import android.content.Context
+import com.parse.FunctionCallback
+import com.parse.ParseCloud
 import com.smlnskgmail.jaman.randomnotes.R
+import kotlinx.android.synthetic.main.dialog_share.*
 
 class ShareDialog(context: Context) : BaseDialog(context) {
 
     override fun initializeDialog() {
+        dialog_share_cancel.setOnClickListener {
+            cancel()
+        }
+        dialog_share_action.setOnClickListener {
+            share()
+        }
+    }
 
+    private fun share() {
+        val shareData = hashMapOf<String, String>()
+        shareData["email"] = dialog_share_email.text.toString()
+        ParseCloud.callFunctionInBackground("invite", shareData,
+            FunctionCallback<Void> { `object`, e ->
+
+            }
+        )
     }
 
     override fun getLayoutResId() = R.layout.dialog_share
