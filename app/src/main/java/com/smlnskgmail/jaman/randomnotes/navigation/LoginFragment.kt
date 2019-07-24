75fa9b6cbb9f8ce8dff52fc0021df557cb788a5b
fartem <jaman.smlnsk@gmail.com>
2019-07-24 17:34:22 +0300

    2019-07-24 Version 0.9.5: Update login functionality

--- app/src/main/java/com/smlnskgmail/jaman/randomnotes/navigation/LoginFragment.kt
+++ app/src/main/java/com/smlnskgmail/jaman/randomnotes/navigation/LoginFragment.kt
@@ -1,17 +1,15 @@
 package com.smlnskgmail.jaman.randomnotes.navigation
 
 import android.content.Intent
-import android.view.View
+import android.os.Bundle
 import com.facebook.AccessToken
 import com.facebook.GraphRequest
 import com.parse.ParseUser
 import com.parse.facebook.ParseFacebookUtils
 import com.smlnskgmail.jaman.randomnotes.MainActivity
 import com.smlnskgmail.jaman.randomnotes.R
-import com.smlnskgmail.jaman.randomnotes.navigation.BaseFragment
 import com.smlnskgmail.jaman.randomnotes.utils.UIUtils
 import kotlinx.android.synthetic.main.fragment_login.*
-import android.os.Bundle
 
 class LoginFragment : BaseFragment() {
 
@@ -19,66 +17,82 @@ class LoginFragment : BaseFragment() {
 
     override fun initialize() {
         register_account.setOnClickListener {
-            if (loginMode) {
-                login_action.text = getString(R.string.action_sign_up)
-                register_account.text = getString(R.string.message_have_account)
-                email.visibility = View.VISIBLE
-            } else {
-                login_action.text = getString(R.string.action_login)
-                register_account.text = getString(R.string.message_not_account)
-                email.visibility = View.GONE
-            }
-            loginMode = !loginMode
+            register()
         }
         login_action.setOnClickListener {
-            if (loginMode) {
-                ParseUser.logInInBackground(username.text.toString(), password.text.toString()) { user, e ->
-                    if (user != null) {
-                        (activity as MainActivity).loginComplete()
-                    } else if (e != null) {
-                        signInError(e)
-                    }
-                }
-            } else {
-                val parseUser = ParseUser()
-                parseUser.username = username.text.toString()
-                parseUser.email = email.text.toString()
-                parseUser.setPassword(password.text.toString())
-                parseUser.signUpInBackground {
-                    if (it == null) {
-                        (activity as MainActivity).loginComplete()
-                    } else {
-                        signInError(it)
-                    }
-                }
-            }
+            loginWithEmail()
         }
         facebook_login.setOnClickListener {
-            ParseFacebookUtils.logInWithReadPermissionsInBackground(this,
-                listOf("public_profile", "email")) { user, e ->
+            loginWithFacebook()
+        }
+    }
+
+    private fun register() {
+        if (loginMode) {
+            login_action.text = getString(R.string.action_sign_up)
+            register_account.text = getString(R.string.message_have_account)
+        } else {
+            login_action.text = getString(R.string.action_login)
+            register_account.text = getString(R.string.message_not_account)
+        }
+        loginMode = !loginMode
+    }
+
+    private fun loginWithEmail() {
+        if (loginMode) {
+            ParseUser.logInInBackground(email.text.toString(), password.text.toString()) { user, e ->
                 if (user != null) {
-                    if (user.email == null) {
-                        val graphRequest = GraphRequest.newMeRequest(AccessToken
-                            .getCurrentAccessToken()) { data, response ->
-                            if (data != null) {
-                                user.email = data.getString("email")
-                                user.saveInBackground {
-                                    if (it == null) {
-                                        (activity as MainActivity).loginComplete()
-                                    }
-                                }
-                            } else {
-                                (activity as MainActivity).loginError()
-                            }
-                        }
-                        val parameters = Bundle()
-                        parameters.putString("fields", "email")
-                        graphRequest.parameters = parameters
-                        graphRequest.executeAsync()
-                    }
+                    (activity as MainActivity).loginComplete()
                 } else if (e != null) {
                     signInError(e)
                 }
+            }
+        } else {
+            val parseUser = ParseUser()
+            parseUser.username = email.text.toString()
+            parseUser.email = email.text.toString()
+            parseUser.setPassword(password.text.toString())
+            parseUser.signUpInBackground {
+                if (it == null) {
+                    (activity as MainActivity).loginComplete()
+                } else {
+                    signInError(it)
+                }
+            }
+        }
+    }
+
+    private fun loginWithFacebook() {
+        val emailField = "email"
+        ParseFacebookUtils.logInWithReadPermissionsInBackground(this,
+            listOf("public_profile", emailField)) { user, e ->
+            if (user != null) {
+                if (user.email == null) {
+                    val graphRequest = GraphRequest.newMeRequest(
+                        AccessToken
+                            .getCurrentAccessToken()) { data, response ->
+                        if (data != null) {
+                            user.email = data.getString(emailField)
+                            user.saveInBackground {
+                                if (it == null) {
+                                    (activity as MainActivity).loginComplete()
+                                } else {
+                                    signInError(it)
+                                }
+                            }
+                        } else {
+                            (activity as MainActivity).loginError()
+                        }
+                    }
+                    val parameters = Bundle()
+                    parameters.putString("fields", emailField)
+                    graphRequest.parameters = parameters
+                    graphRequest.executeAsync()
+                } else {
+                    (activity as MainActivity).loginComplete()
+                }
+            } else if (e != null) {
+                signInError(e)
             }
         }
     }
--- app/src/main/java/com/smlnskgmail/jaman/randomnotes/navigation/MainFragment.kt
+++ app/src/main/java/com/smlnskgmail/jaman/randomnotes/navigation/MainFragment.kt
@@ -1,5 +1,7 @@
 package com.smlnskgmail.jaman.randomnotes.navigation
 
+import com.parse.ParseObject
+import com.parse.ParseQuery
 import com.parse.ParseUser
 import com.smlnskgmail.jaman.randomnotes.R
 import com.smlnskgmail.jaman.randomnotes.components.bottomsheets.addnote.AddNoteBottomSheet
@@ -7,7 +9,6 @@ import com.smlnskgmail.jaman.randomnotes.components.bottomsheets.addnote.AddNote
 import com.smlnskgmail.jaman.randomnotes.components.dialogs.ShareDialog
 import com.smlnskgmail.jaman.randomnotes.components.noteslist.NotesAdapter
 import com.smlnskgmail.jaman.randomnotes.entities.Note
-import com.smlnskgmail.jaman.randomnotes.parse.ParseApi
 import com.smlnskgmail.jaman.randomnotes.utils.UIUtils
 import kotlinx.android.synthetic.main.fragment_main.*
 
@@ -28,41 +29,88 @@ class MainFragment : BaseFragment(), AddNoteListener {
 
     private fun initializeFabMenu() {
         share_access.setOnClickListener {
-            collapseMenuAndRun {
-                actionWithNotes {
-                    val shareDialog = ShareDialog(context!!)
-                    shareDialog.show()
-                }
+            actionWithNotes {
+                share()
             }
         }
         restore_notes.setOnClickListener {
-            collapseMenuAndRun {
-                actionWithNotes {
-                    ParseApi.restoreNotesFromServer(dataNotes) { success ->
-                        if (success) {
-                            refreshNotes()
-                        } else {
-
-                        }
-                    }
-                }
+            actionWithNotes {
+                restore()
             }
         }
         sync_notes.setOnClickListener {
-            collapseMenuAndRun {
-                actionWithNotes {
-                    ParseApi.syncNotesWithServer(dataNotes)
-                }
+            actionWithNotes {
+                sync()
             }
         }
         add_note.setOnClickListener {
             collapseMenuAndRun {
-                val addNoteBottomSheet = AddNoteBottomSheet()
-                addNoteBottomSheet.addNoteCreationCallback(this)
-                addNoteBottomSheet.show(activity!!.supportFragmentManager,
-                    addNoteBottomSheet.javaClass.name)
+                addNote()
             }
         }
+    }
+
+    private fun share() {
+        val shareDialog = ShareDialog(context!!)
+        shareDialog.show()
+    }
+
+    private fun restore() {
+        val parseQuery: ParseQuery<ParseObject> = ParseQuery.getQuery(Note.TABLE_NOTE)
+        val parseToSave = mutableListOf<ParseObject>()
+        parseQuery.findInBackground { objects, e ->
+            if (objects.isNotEmpty()) {
+                Note.deleteAllNotes()
+                for (parseData in objects) {
+                    val id = parseData.getLong(Note.COLUMN_ID)
+                    var note = dataNotes.lastOrNull {
+                        it.id == id
+                    }
+                    if (note == null) {
+                        note = Note()
+                    }
+                    if (note.parseObjectId == null) {
+                        note.parseObjectId = parseData.objectId
+                    }
+                    val parseNote = note.restoreFromParseObject(parseData)
+                    note.save()
+                    parseNote.put(Note.COLUMN_ID, note.id)
+                    parseToSave.add(parseNote)
+                }
+                ParseObject.saveAllInBackground(parseToSave) {
+                    if (it == null) {
+                        refreshNotes()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun sync() {
+        val objectsToSave = mutableListOf<ParseObject>()
+        for (note in dataNotes) {
+            objectsToSave.add(note.getParseObject())
+        }
+        ParseObject.saveAllInBackground(objectsToSave) {
+            if (it == null) {
+                for (savedNote in objectsToSave) {
+                    val note = dataNotes.firstOrNull { noteInList ->
+                        noteInList.id == savedNote.get(Note.COLUMN_ID)
+                    }
+                    if (note != null) {
+                        note.parseObjectId = savedNote.objectId
+                        note.save()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addNote() {
+        val addNoteBottomSheet = AddNoteBottomSheet()
+        addNoteBottomSheet.addNoteCreationCallback(this)
+        addNoteBottomSheet.show(activity!!.supportFragmentManager,
+            addNoteBottomSheet.javaClass.name)
     }
 
     private fun collapseMenuAndRun(action: () -> Unit) {
@@ -71,10 +119,12 @@ class MainFragment : BaseFragment(), AddNoteListener {
     }
 
     private fun actionWithNotes(action: () -> Unit) {
-        if (ParseUser.getCurrentUser() != null) {
-            action()
-        } else {
-            UIUtils.toast(context!!, getString(R.string.message_sign_in))
+        collapseMenuAndRun {
+            if (ParseUser.getCurrentUser() != null) {
+                action()
+            } else {
+                UIUtils.toast(context!!, getString(R.string.message_sign_in))
+            }
         }
     }
 
